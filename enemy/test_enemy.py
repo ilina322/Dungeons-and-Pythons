a7400344f5e022b1f75155c913734afbd761440c
Ilina Pavlova <ilina.pavllova@gmail.com>
2019-04-10 19:32:16 +0300

    add dungeon

--- enemy/test_enemy.py
+++ enemy/test_enemy.py
@@ -1,13 +1,21 @@
 import unittest
+import sys
+sys.path.insert(0, '../')
 from enemy import *
+from weapon import *
 class TestEnemy(unittest.TestCase):
 
-    e = Enemy(health=20, mana=10, damage=10)
 
     def test_init_values(self):
+        e = Enemy(health=20, mana=10, damage=10)
         self.assertEqual(e.health, 20)
         self.assertEqual(e.mana, 10)
         self.assertEqual(e.damage, 10)
+
+    def test_attack_if_weapon_is_available_then_return_weapon_damage(self):
+        e = Enemy(health=20, mana=10, damage=10)
+        w = Weapon()
+
 
 if __name__ == '__main__':
     unittest.main()
