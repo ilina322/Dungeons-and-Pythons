c235f69d7196d262af0d5b114989069e46edcbf8
bogipeneva <bogomilapeneva@gmail.com>
2019-04-11 18:57:32 +0300

    Adding can_move, find_hero_position, move_hero and dungeon tests

--- dungeon/dungeon.py
+++ dungeon/dungeon.py
@@ -4,17 +4,22 @@ sys.path.insert(0, '../hero')
 from hero import *
 
 class Dungeon:
-    def __init__(self):
+    def __init__(self, file_name):
+        self.file_name = file_name
         self._map = self.create_map()
 
     def create_map(self):
-        f = open('level.txt', 'r')
+        f = open(self.file_name, 'r')
         map_str = f.read()
         map_lst = map_str.split('\n')
+        if len(map_lst) == 1 and len(map_lst[0]) == 0:
+            return []
+
         map_list = []
         for row in map_lst:
             list_col = [sympol for sympol in row]
             map_list.append(list_col)
+
         return map_list
 
 
@@ -31,11 +36,55 @@ class Dungeon:
                     return True
         return False
 
+    def can_move(self, row, col):
+
+        if col >= len(self._map[0]) or col < 0 or row >= len(self._map) or row < 0 or self._map[row][col] == '#':
+            return False
+
+        elif self._map[row][col] == 'T':
+            return 'Found treasure!'
+
+        elif self._map[row][col] == 'E':
+            return 'Start a fight!'
+
+        return True
+
+    def find_hero_position(self):
+        for row in range(len(self._map)):
+            for col in range(len(self._map[0])):
+                if self._map[row][col] == 'H':
+                    return (row, col)
+
+    def move_hero(self, direction):
+        if direction == 'up':
+            curr_hero_position = self.find_hero_position()
+            can_move_on = self.can_move(curr_hero_position[0] - 1, curr_hero_position[1])
+            new_hero_position = (curr_hero_position[0] - 1, curr_hero_position[1])
+
+        elif direction == 'down':
+            curr_hero_position = self.find_hero_position()
+            can_move_on = self.can_move(curr_hero_position[0] + 1, curr_hero_position[1])
+            new_hero_position = (curr_hero_position[0] + 1, curr_hero_position[1])
+
+        elif direction == 'left':
+            curr_hero_position = self.find_hero_position()
+            can_move_on = self.can_move(curr_hero_position[0], curr_hero_position[1] - 1)
+            new_hero_position = (curr_hero_position[0], curr_hero_position[1] - 1)
+
+        else:
+            curr_hero_position = self.find_hero_position()
+            can_move_on = self.can_move(curr_hero_position[0], curr_hero_position[1] + 1)
+            new_hero_position = (curr_hero_position[0], curr_hero_position[1] + 1)
+
+        if can_move_on:
+            self._map[curr_hero_position[0]][curr_hero_position[1]] = '.'
+            self._map[new_hero_position[0]][new_hero_position[1]] = 'H'
+
+        return can_move_on
 
 
-d = Dungeon()
 
-d.spawn()
-d.print_map()
+
+
 
 
---
+++ dungeon/test_dungeon.py
@@ -0,0 +1,83 @@
+import unittest
+import dungeon
+from dungeon import *
+
+class TestClassDungeon(unittest.TestCase):
+    def test_initial_value(self):
+        d = Dungeon('level.txt')
+        assert d.file_name == 'level.txt'
+
+    def test_create_map_when_file_attribute_is_empty_then_retutns_empty_list(self):
+        d = Dungeon('empty.txt')
+        self.assertEqual(d.create_map(), [])
+
+    def test_create_map_when_file_attribute_is_not_empty_then_loads_map_from_it_and_returns_list_of_rows(self):
+        d = Dungeon('level.txt')
+        expected_result = [['.', '.', '#', '#', '.', '.', 'S', '.', '.', 'T'], ['#', 'T', '#', '#', 'S', '.', '#', '#', '#', '.'], ['#', '.', '#', '#', '#', 'E', '#', '#', '#', 'E'], ['#', '.', 'E', '.', '.', '.', '#', '#', '#', '.'], ['#', '#', '#', 'T', '#', '#', '#', '#', '#', 'G']]
+        self.assertEqual(d.create_map(), expected_result)
+
+    def test_spawn_when_there_is_no_S_location_on_the_map_then_return_false(self):
+        d = Dungeon('empty.txt')
+        d._map = [['.', '.', '#', '#', '.', '.', '#', '.', '.', 'T'], ['#', 'T', '#', '#', '#', '.', '#', '#', '#', '.'], ['#', '.', '#', '#', '#', 'E', '#', '#', '#', 'E'], ['#', '.', 'E', '.', '.', '.', '#', '#', '#', '.'], ['#', '#', '#', 'T', '#', '#', '#', '#', '#', 'G']]
+        self.assertEqual(d.spawn(), False)
+
+    def test_spawn_when_there_are_S_locations_on_the_map_then_spawn_hero_in_first_S_location_and_return_true(self):
+        d = Dungeon('level.txt')
+        assert d.spawn() == True
+        expected_result = [['.', '.', '#', '#', '.', '.', 'H', '.', '.', 'T'], ['#', 'T', '#', '#', 'S', '.', '#', '#', '#', '.'], ['#', '.', '#', '#', '#', 'E', '#', '#', '#', 'E'], ['#', '.', 'E', '.', '.', '.', '#', '#', '#', '.'], ['#', '#', '#', 'T', '#', '#', '#', '#', '#', 'G']]
+        self.assertEqual(d._map, expected_result)
+
+    def test_can_move_when_is_passed_coordinat_that_is_out_of_range_then_return_false(self):
+        d = Dungeon('level.txt')
+        self.assertEqual(d.can_move(-1, 7), False)
+
+    def test_can_move_when_you_try_to_move_into_an_obstacle_then_return_false(self):
+        d = Dungeon('level.txt')
+        self.assertEqual(d.can_move(0, 3), False)
+
+
+    def test_can_move_when_you_try_to_move_into_an_enemy_then_return_message_that_a_figth_is_started(self):
+        d = Dungeon('level.txt')
+        self.assertEqual(d.can_move(2, 9), 'Start a fight!')
+
+    def test_can_move_when_you_try_to_move_into_treasure_then_return_message_that_a_treasure_is_found(self):
+        d = Dungeon('level.txt')
+        self.assertEqual(d.can_move(1, 1), 'Found treasure!')
+
+    def test_find_hero_position_that_returns_tuple_with_current_hero_position(self):
+        d = Dungeon('level.txt')
+        d.spawn()
+        self.assertEqual(d.find_hero_position(), (0, 6))
+
+    def test_move_hero_when_is_passed_direction_that_will_move_him_into_a_obstacle_then_return_false(self):
+        d = Dungeon('level.txt')
+        d.spawn()
+        self.assertEqual(d.move_hero('down'), False)
+
+
+    def test_move_hero_when_is_passed_direction_that_will_move_him_into_an_enemy_then_return_message_that_a_figth_is_started(self):
+        d = Dungeon('level.txt')
+        d.spawn()
+        d.move_hero('left')
+        d.move_hero('down')
+        self.assertEqual(d.move_hero('down'), 'Start a fight!')
+
+    def test_hero_move_when_is_passed_direction_that_will_move_him_into_treasure_then_return_message_that_a_treasure_is_found(self):
+        d = Dungeon('level.txt')
+        d.spawn()
+        d.move_hero('right')
+        d.move_hero('right')
+        self.assertEqual(d.move_hero('right'), 'Found treasure!')
+
+    def test_move_hero_when_is_passed_direction_that_will_move_him_outside_the_map_then_return_false(self):
+        d = Dungeon('level.txt')
+        d.spawn()
+        self.assertEqual(d.move_hero('up'), False)
+
+
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
