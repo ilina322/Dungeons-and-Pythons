f61bd5fc075a8dce28e25aba9510572a6e4b32fa
Ilina Pavlova <ilina.pavllova@gmail.com>
2019-04-13 22:57:16 +0300

    update fight

--- fight/fight.py
+++ fight/fight.py
@@ -16,24 +16,24 @@ class Fight:
             if self.hero.spell != None:
                 if self.hero.weapon != None:
                     if self.hero.weapon.damage > self.hero.spell.damage:
-                        text = "Hero hits enemy with {}! ".format(self.hero.weapon.name)
+                        text = "{0} hits enemy with {1}! ".format(self.hero.known_as(), self.hero.weapon.name)
                         damage = self.hero.attack(by='weapon')
                     else:
                         if self.hero.mana < self.hero.spell.mana_cost:
-                             text = 'Hero has not enough mana! Use {}! '.format(self.hero.weapon.name)
+                             text = '{0} has not enough mana! Use {1}! '.format(self.hero.known_as(), self.hero.weapon.name)
                              damage = self.hero.attack(by='weapon')
                         else:
-                            text = "Hero casts {}! ".format(self.hero.spell.name)
+                            text = "{0} casts {1}! ".format(self.hero.known_as(), self.hero.spell.name)
                             damage = self.hero.attack(by='spell')
                 else:
                     if self.hero.mana < self.hero.spell.mana_cost:
-                             text = 'Hero has not enough  mana and no weapon! '
+                             text = '{} has not enough mana and no weapon! '.format(self.hero.known_as())
                     else:
-                        text = "Hero casts {}! ".format(self.hero.spell.name)
+                        text = "{0} casts {1}! ".format(self.hero.known_as(), self.hero.spell.name)
                         damage = self.hero.attack(by='spell')
             else:
                 if self.hero.weapon != None:
-                    text = "Hero hits enemy with {}! ".format(self.hero.weapon.name)
+                    text = "{0} hits enemy with {1}! ".format(self.hero.known_as(), self.hero.weapon.name)
                     damage = self.hero.attack(by='weapon')
             self.enemy.take_damage(damage)
             if(self.enemy.is_alive()):
@@ -45,11 +45,11 @@ class Fight:
     def enemy_attack(self):
         if self.enemy.is_alive():
             damage = self.enemy.damage
-            text = 'Enemy hits hero for {} damage! '.format(damage)
+            text = 'Enemy hits {0} for {1} damage! '.format(self.hero.known_as(), damage)
             if self.enemy.spell != None:
                 if self.enemy.weapon != None:
                     if self.enemy.weapon.damage > self.enemy.spell.damage:
-                        text = "Enemy hits hero with {}! ".format(self.enemy.weapon.name)
+                        text = "Enemy hits {0} with {1}! ".format(self.hero.known_as(), self.enemy.weapon.name)
                         damage = self.enemy.attack(by='weapon')
                     else:
                         if self.enemy.mana < self.enemy.spell.mana_cost:
@@ -64,13 +64,13 @@ class Fight:
                         damage = self.enemy.attack(by='spell')
             else:
                 if self.enemy.weapon != None:
-                    text = "Enemy hits hero with {}! ".format(self.enemy.weapon.name)
+                    text = "Enemy hits {0} with {1}! ".format(self.hero.known_as(), self.enemy.weapon.name)
                     damage = self.enemy.attack(by='weapon')
             self.hero.take_damage(damage)
             if(self.hero.is_alive()):
-                text += "Hero health is: {}".format(self.hero.health)
+                text += "{0} health is: {1}".format(self.hero.known_as(), self.hero.health)
             else:
-                text += "Hero is dead!"
+                text += "{} is dead!".format(self.hero.known_as())
             print(text)
 
     def start(self):
@@ -78,11 +78,19 @@ class Fight:
             self.hero_attack()
             self.enemy_attack()
 
+    def hero_wins(self):
+        if self.enemy.is_alive() and not self.hero.is_alive():
+            return False
+        elif not self.enemy.is_alive() and self.hero.is_alive():
+            return True
+        return False
+
+
 w = Weapon(name='Sword', damage=20)
 s = Spell(name='Fireball', damage=30, mana_cost=20, cast_range=2)
 h = Hero(name="Bron", title="Dragonslayer", health=100, mana=100, mana_regeneration_rate=2)
-e = Enemy(health=100, mana=100, damage=10, mana_regeneration_rate=2)
-h.learn(s)
-e.equip(w)
+e = Enemy(health=100, mana=150, damage=10)
+e.learn(s)
+h.equip(w)
 f = Fight(h, e)
 f.start()
